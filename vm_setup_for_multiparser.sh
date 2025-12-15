b058e53c960d079604239d1244a0b9278f5a0039
Asif Mahmud <1421015@iub.edu.bd>
2025-12-15 14:47:36 +0600

    [in progress] update readme for parser

--- vm_setup_for_multiparser.sh
+++ vm_setup_for_multiparser.sh
@@ -15,7 +15,9 @@ sudo apt update -y
 
 echo "Follow official documentation: https://tesseract-ocr.github.io/tessdoc/Installation.html"
 
+echo "Installing tesseract-ocr and libtesseract-dev..."
 sudo apt install tesseract-ocr
 sudo apt install libtesseract-dev
+echo "tesseract-ocr and libtesseract-dev installed successfully..."
 
 echo "Setup completed successfully !!"
